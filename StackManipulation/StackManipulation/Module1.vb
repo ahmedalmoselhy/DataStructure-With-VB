7a1543b5b6682e5ad2ea7c1878a331d7b6d46ae2
ahmedalmoselhy <ahmedalmoselhy.slm@gmail.com>
2019-01-05 14:46:04 +0200

    creating the stack class

---
+++ StackManipulation/StackManipulation/Module1.vb
@@ -0,0 +1,54 @@
+﻿Module Module1
+    Class myStack
+        Private ToS As Integer
+        Private list As New ArrayList()
+        ' constructor to set ToS to -1
+        Public Sub New()
+            ToS = -1
+        End Sub
+
+        ' a function to return the number of elements in the stack
+        Function count() As Integer
+            Return list.Count
+        End Function
+
+        ' a method to insert new items in the stack
+        Public Sub push(ByVal value As Object)
+            list.Add(value)
+            ToS += 1
+        End Sub
+
+        ' a function to remove the top of stack and return with it
+        Public Function pop()
+            If isEmpty() Then
+                Return -1
+            End If
+            Dim obj As Object = list.Item(ToS)
+            list.RemoveAt(ToS)
+            ToS -= 1
+            Return obj
+        End Function
+
+        ' a method to clear the stack
+        Public Sub clear()
+            list.Clear()
+            ToS = -1
+        End Sub
+
+        ' a function to return the TOS without removing it
+        Public Function peek()
+            Return list.Item(ToS)
+        End Function
+
+        Public Function isEmpty()
+            If ToS = -1 Then
+                Return True
+            End If
+            Return False
+        End Function
+    End Class
+    Sub Main()
+
+    End Sub
+
+End Module
