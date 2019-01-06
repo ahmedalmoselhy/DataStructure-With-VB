d975ecd3e99a7b7dc79a6145030fb3a908a8872c
ahmedalmoselhy <ahmedalmoselhy.slm@gmail.com>
2019-01-06 18:38:13 +0200

    added vertex class

--- UndirectedWeightedGraph/UndirectedWeightedGraph/Module1.vb
+++ UndirectedWeightedGraph/UndirectedWeightedGraph/Module1.vb
@@ -1,5 +1,12 @@
 ﻿Module Module1
-
+    Class MyVertex
+        Public wasVisited As Boolean
+        Public label As String
+        Public Sub New(ByVal label As String)
+            Me.label = label
+            wasVisited = False
+        End Sub
+    End Class
     Sub Main()
 
     End Sub
